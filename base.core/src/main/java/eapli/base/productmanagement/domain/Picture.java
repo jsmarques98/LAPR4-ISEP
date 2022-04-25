933573bca6b54d5a6016acce083c877874aa885a
LuisFerreira_1201008 <1201008@isep.ipp.pt>
2022-04-25 22:51:08 +0100

    missed file last commit

---
+++ base.core/src/main/java/eapli/base/productmanagement/domain/Picture.java
@@ -0,0 +1,29 @@
+package eapli.base.productmanagement.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Embeddable;
+import java.util.List;
+
+@Embeddable
+public class Picture implements ValueObject {
+    private String picturePaths;
+
+    public Picture(String picturePaths){
+        this.picturePaths = picturePaths;
+    }
+
+    public Picture(){
+        // empty constructor
+    }
+
+    public String getPicturePath() {
+        return picturePaths;
+    }
+
+    public static Picture valueOf(final String path){
+        return new Picture(path);
+    }
+
+
+}
